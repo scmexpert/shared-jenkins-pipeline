7abe22db91ccfc0095075ed65ee8199f1609c1ba
Kirubakaran Kuppusamy <scmmeeting@gmail.com>
2020-11-11 13:50:02 +0000

    Update samplePipeline.groovy

--- vars/samplePipeline.groovy
+++ vars/samplePipeline.groovy
@@ -5,6 +5,6 @@ def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        userRemoteConfigs: [[credentialsId: stageParams.sshkey, url: stageParams.url ]]
     ])
   }
